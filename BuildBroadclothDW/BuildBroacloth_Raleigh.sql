a9f43d40e1324d500ad074bb062697976603ff4d
Raleigh Burrell <raleighburrell@gmail.com>
2017-10-05 11:37:01 -0600

    My Portion of the SQL File

--- BuildBroadclothDW/BuildBroacloth_Raleigh.sql
+++ BuildBroadclothDW/BuildBroacloth_Raleigh.sql
@@ -4,3 +4,52 @@
 --Created October 5th, 2017
 
 --Creating the Data Warehouse
+IF NOT EXISTS(SELECT * FROM sys.databases  --only adds if it doesn't exist
+	WHERE name = N'DWBroadcloth')
+	CREATE DATABASE DWBroadcloth
+GO
+USE DWBroadcloth
+GO
+
+--Checking for Existing Tables
+IF EXISTS( --removes a table if it exists
+	SELECT *
+	FROM sys.tables
+	WHERE name = N'FactBatch'
+	)
+	DROP TABLE FactBatch;
+
+IF EXISTS(
+	SELECT *
+	FROM sys.tables
+	WHERE name = N'DimDate'
+	)
+	DROP TABLE DimDate;
+
+IF EXISTS(
+	SELECT *
+	FROM sys.tables
+	WHERE name = N'DimShipment'
+	)
+	DROP TABLE DimShipment;
+
+IF EXISTS(
+	SELECT *
+	FROM sys.tables
+	WHERE name = N'DimCompliance'
+	)
+	DROP TABLE DimCompliance;
+
+IF EXISTS(
+	SELECT *
+	FROM sys.tables
+	WHERE name = N'DimOrder'
+	)
+	DROP TABLE DimOrder;
+
+IF EXISTS(
+	SELECT *
+	FROM sys.tables
+	WHERE name = N'DimItem'
+	)
+	DROP TABLE DimItem;
