68b6a3f5eb6444de487c2393e381a66bf654ff99
slewis20 <32173090+slewis20@users.noreply.github.com>
2017-10-15 15:16:02 -0600

    DimFactory

--- BroadClothSolution/Build DW/BroadclothDW ETL/BroadclothDW ETL Code.sql
+++ BroadClothSolution/Build DW/BroadclothDW ETL/BroadclothDW ETL Code.sql
@@ -24,3 +24,14 @@ INNER JOIN Broadcloth.dbo.WorkerCheck
 INNER JOIN Broadcloth.dbo.WorkingConditions
 	ON WorkerCheck.ComplianceID = WorkingConditions.ComplianceID
 GROUP BY Compliance.ComplianceID;
+
+--DimFactory
+SELECT
+	Factory_AK = CAST (FactoryID AS INT),
+	Nation = CAST (Nation AS NVARCHAR(50)),
+	[State] = CAST ([State] AS NVARCHAR (50)),
+	City = CAST (City AS NVARCHAR (30)),
+	GMTDifference = CAST (GMTDifference AS DECIMAL(18,0)),
+	MaxWorkers = CAST (MaxWorkers AS INT),
+	BaseCurrency = CAST (BaseCurrency AS NVARCHAR(5))
+FROM Broadcloth.dbo.Factory;
