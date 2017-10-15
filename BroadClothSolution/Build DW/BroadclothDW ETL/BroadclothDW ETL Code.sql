2172fcadd75277db3589e30886240e5d79a72e50
Raleigh Burrell <raleighburrell@gmail.com>
2017-10-15 15:14:33 -0600

    ETL SQL File
    Look in DW Build Folder/ETL somthing or other

---
+++ BroadClothSolution/Build DW/BroadclothDW ETL/BroadclothDW ETL Code.sql
@@ -0,0 +1,26 @@
+--BroadclothDW ETL Scripts to load all tables
+--Created by Raleigh, Sydney, Cornel, and Aleksandra
+
+--DimShipment
+SELECT 
+	ShipmentAK = CAST(ShipmentID AS INT),
+	Nation = CAST(ShipNation AS NVARCHAR(50)),
+	State = CAST(ShipState AS NVARCHAR(20)),
+	City = CAST(ShipCity AS NVARCHAR(30)),
+	ShippingCurrency = CAST(ShipCurrency AS NVARCHAR(5))
+FROM Broadcloth.dbo.Shipment;
+
+--DimCompliance
+SELECT
+	ComplianceAK = Compliance.ComplianceID,
+	--WorkingConditions.ConditionCategory,
+	OverallComplianceRating = CAST(AVG(Compliance.OverallRating) AS DECIMAL(18,0)),
+	AverageAge = CAST(AVG(WorkerCheck.Age) AS DECIMAL(18,0)),
+	--WorkerCheck.AgeDocuments,
+	PercentFemale = CAST((CAST(SUM(CASE WHEN WorkerCheck.Gender LIKE 'FEMALE' THEN 1 ELSE 0 END) AS DECIMAL) / CAST(COUNT(WorkerCheck.Gender) AS DECIMAL)) AS DECIMAL(9,6))
+FROM Broadcloth.dbo.Compliance
+INNER JOIN Broadcloth.dbo.WorkerCheck
+	ON Compliance.ComplianceID = WorkerCheck.ComplianceID
+INNER JOIN Broadcloth.dbo.WorkingConditions
+	ON WorkerCheck.ComplianceID = WorkingConditions.ComplianceID
+GROUP BY Compliance.ComplianceID;
