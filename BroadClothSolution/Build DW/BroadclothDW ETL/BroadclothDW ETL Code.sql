7a368c870d25e70b4f5dc3aa14bd86cc699e87da
Raleigh Burrell <raleighburrell@gmail.com>
2017-10-15 15:59:03 -0600

    ok guys sorry

--- BroadClothSolution/Build DW/BroadclothDW ETL/BroadclothDW ETL Code.sql
+++ BroadClothSolution/Build DW/BroadclothDW ETL/BroadclothDW ETL Code.sql
@@ -29,9 +29,20 @@ GROUP BY Compliance.ComplianceID;
 SELECT
 	Factory_AK = CAST (FactoryID AS INT),
 	Nation = CAST (Nation AS NVARCHAR(50)),
-	[State] = CAST ([State] AS NVARCHAR (50)),
 	City = CAST (City AS NVARCHAR (30)),
 	GMTDifference = CAST (GMTDifference AS DECIMAL(18,0)),
 	MaxWorkers = CAST (MaxWorkers AS INT),
 	BaseCurrency = CAST (BaseCurrency AS NVARCHAR(5))
 FROM Broadcloth.dbo.Factory;
+
+--DimItem
+SELECT
+	Item_AK = CAST(ItemID AS NVARCHAR(60)),
+	ModelDescription = CAST(ModelID AS NVARCHAR(250)),
+	BasePrice = CAST( BasePrice AS DECIMAL (9,4)),
+	ListPrice = CAST(ListPrice AS NUMERIC(338,4)),
+	Color = CAST( Color AS NVARCHAR (20)),
+	ItemSize = CAST( ItemSize AS NUMERIC(18,0))
+FROM Broadcloth.dbo.Item
+INNER JOIN Broadcloth.dbo.Model
+ON Item.ItemID = Model.ItemID;
