5be0486b2758b919e05e3c7e216840ba2120681b
abratic <aleksandra05@msn.com>
2017-10-22 22:32:59 -0600

    Revert "delete state"
    This reverts commit 7e320ff5aaacc73a404a5fed69e0e2daf3f2d52d.

--- BroadClothSolution/Build DW/BroadclothDW ETL/BroadclothDW ETL Code.sql
+++ BroadClothSolution/Build DW/BroadclothDW ETL/BroadclothDW ETL Code.sql
@@ -29,6 +29,7 @@ GROUP BY Compliance.ComplianceID;
 SELECT
 	Factory_AK = CAST (FactoryID AS INT),
 	Nation = CAST (Nation AS NVARCHAR(50)),
+	[State] = CAST ([State] AS NVARCHAR (50)),
 	City = CAST (City AS NVARCHAR (30)),
 	GMTDifference = CAST (GMTDifference AS DECIMAL(18,0)),
 	MaxWorkers = CAST (MaxWorkers AS INT),
